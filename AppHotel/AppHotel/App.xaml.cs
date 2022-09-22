c10b42ac0a9bc8daefc6ccb8c2c4776740886e12
moraisbianca <biancacamilamorais@gmail.com>
2022-09-22 14:04:06 -0300

    Adicionar arquivos de projeto.

---
+++ AppHotel/AppHotel/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+
+using System.Threading;
+using System.Globalization;
+
+using AppHotel.Model;
+using AppHotel.View;
+
+
+namespace AppHotel
+{
+    public partial class App : Application
+    {
+        public List<Suite> lista_suites = new List<Suite>
+        {
+            new Suite()
+            {
+               Descricao = "Ricos",
+               DiariaAdulto = 150.0,
+               DiariaCrianca = 75.0
+            },
+            new Suite()
+            {
+               Descricao = "Classe Média",
+               DiariaAdulto = 130.0,
+               DiariaCrianca = 65.0
+            },
+            new Suite()
+            {
+               Descricao = "Pobres",
+               DiariaAdulto = 70.0,
+               DiariaCrianca = 35.0
+            }
+        };
+        public App()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
+            InitializeComponent();
+            MainPage = new NavigationPage(new ContratacaoHospedagem());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppHotel/AppHotel/Model/Hospedagem.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//ok
+namespace AppHotel.Model
+{
+
+    //Transportando dados
+    public class Hospedagem
+    {
+        int quantidade_adultos;
+        Suite Quarto;
+
+        public Suite QuartoEscolhido
+        {
+            //Pega a informação do quarto escolhido (erro caso não haja uma opção selecionada)
+            get => Quarto;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Erro! Informe o quarto escolhido.");
+
+                    Quarto = value;
+                }
+            }
+        }
+
+        public int QntAdultos
+        {
+            //Pega a informação da quantidade de adultos (erro caso não haja uma opção selecionada)
+            get => quantidade_adultos;
+            set
+            {
+                if (value == 0)
+                    throw new Exception("Erro! Informe a quantidade de adultos.");
+
+                quantidade_adultos = value;
+            }
+        }
+
+        //Não necessita de uma mensagem de erro (a seleção de crianças não é obrigatória)
+        public int QntCriancas { get; set; }
+
+        //Pega os dados do date picker (Entrada/Saída) e será usado na ESTADIA
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+        public int Estadia
+        {
+            get
+            {
+                //Retorna a quantidade de dias no hotel de acordo com os dados do date picker
+                return DataCheckOut.Subtract(DataCheckIn).Days;
+            }
+        }
+
+        //Faz o cálculo de acordo com quarto escolhido, quantidade de pessoas e tempo de estadia
+        public double ValorTotal
+        {
+            get => ((QntAdultos * QuartoEscolhido.DiariaAdulto) +
+                     (QntCriancas * QuartoEscolhido.DiariaCrianca)
+                   ) * Estadia;
+        }
+
+    }
+}
